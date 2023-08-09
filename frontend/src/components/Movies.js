5b3702b4902e54e61410e215ef9fc3b810a709d2
Carlos Fernandez <cafl.23w@gmail.com>
2023-08-09 15:56:54 +0200

    add fetch to get movies from backend

--- frontend/src/components/Movies.js
+++ frontend/src/components/Movies.js
@@ -4,30 +4,26 @@ import { Link } from "react-router-dom";
 const Movies = () => {
     const [movies, setMovies] = useState([]);
 
-    useEffect( () => {
-        let moviesList = [
-            {
-                id: 1,
-                title: "Highlander",
-                release_date: "1986-03-07",
-                runtime: 116,
-                mpaa_rating: "R",
-                description: "Some long description",
-            },
-            {
-                id: 2,
-                title: "Raiders of the Lost Ark",
-                release_date: "1981-06-12",
-                runtime: 115,
-                mpaa_rating: "PG-13",
-                description: "Some long description",
-            },
-        ];
+    useEffect(() => {
+        const headers = new Headers();
+        headers.append("Content-Type", "application/json");
 
-        setMovies(moviesList)
+        const requestOptions = {
+            method: "GET",
+            headers: headers,
+        };
+
+        fetch(`http://localhost:8080/movies`, requestOptions)
+            .then((response) => response.json())
+            .then((data) => {
+                setMovies(data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
     }, []);
 
-    return(
+    return (
         <div>
             <h2>Movies</h2>
             <hr />
@@ -43,18 +39,16 @@ const Movies = () => {
                     {movies.map((m) => (
                         <tr key={m.id}>
                             <td>
-                                <Link to={`/movies/${m.id}`}>
-                                    {m.title}
-                                </Link>
+                                <Link to={`/movies/${m.id}`}>{m.title}</Link>
                             </td>
                             <td>{m.release_date}</td>
                             <td>{m.mpaa_rating}</td>
-                        </tr>    
+                        </tr>
                     ))}
                 </tbody>
             </table>
         </div>
-    )
-}
+    );
+};
 
 export default Movies;
